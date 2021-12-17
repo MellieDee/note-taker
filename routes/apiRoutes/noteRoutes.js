4fcd8fcdf3aab38a3aad22cb9f7113b6f08d9d16
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-12-16 22:10:02 -0800

    test uniqid

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,32 +1,48 @@
 const fs = require('fs');
+const uniqid = require('uniqid');
+
 const router = require('express').Router();
 
 // require data file db.json
 const notes = require('../../db/db.json');
+const { createNewNote, findById } = require('../../lib/notes');
 
 
 // To GET notes data
 router.get('/notes', (req, res) => {
-  
-   if (notes) {
-     res.json(notes);
-     console.log(notes);
-   } else {
+
+  if (notes) {
+    res.json(notes);
+    console.log(notes);
+  } else {
     res.status(404).send('Something is wrong');
-   }
+  }
 })
 
 
 // GET by ID
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  res.json(result)
+  // if (notes) {
+  //   res.json(notes);
+  //   console.log(notes);
+  // } else {
+  //  res.status(404).send('Something is wrong');
+  // }
+})
 
 
-// to POST a new note
+// // to POST a new note
+
 
 router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be (req.body is the content)
-    req.body.id = notes.length.toString();
-    req.json(req.body);
-    console.log(req.json(req.body));
+  //req.body is where incoming data will go
+  
+  // create uniqId
+  req.body.id = uniqid.process();
+  res.json(req.body);
+  console.log(req.json(req.body));
 })
 
 
