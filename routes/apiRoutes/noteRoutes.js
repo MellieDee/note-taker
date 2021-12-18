ed1f52d1c39834e09dcc7b46efe2823fbe590aec
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-12-18 14:38:46 -0800

    get post functioning

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,37 +1,61 @@
 const fs = require('fs');
-const path = require('path');
-const uniqid = require('uniqid');
-
 const router = require('express').Router();
+const uniqid = require('uniqid');
+const findById = require('../../lib/notes');
 
-// require data file db.json
-const notes = require('../../db/db.json');
 
 
 // To GET notes data
 router.get('/notes', (req, res) => {
-
+  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
   if (notes) {
+    // console.log('notes are: ' + JSON.stringify(notes));
     res.json(notes);
-    console.log(notes);
+    // console.log(notes);
   } else {
     res.status(404).send('Something is wrong');
   }
 })
 
 
-// GET by ID
-// router.get('/notes/:id', (req, res) => {
-//   const result = findById(req.params.id, notes);
-//   res.json(result)
-//   // if (notes) {
-//   //   res.json(notes);
-//   //   console.log(notes);
-//   // } else {
-//   //  res.status(404).send('Something is wrong');
-//   // }
-// })
 
+// GET by ID
+router.get('/notes/:id', (req, res) => {
+  // const notes = fs.readFileSync('db/db.json', 'utf-8')
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// GET by ID
+// router.get('/note/:id', (req, res) => {
+//   const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+
+//   console.log(notes);
+
+//   if (req.params.id) {
+//     // Log  request to the terminal
+//     // console.info(`${req.method} request received to find a note`);
+
+//     // Log the request body
+//     console.info(req.params.id);
+
+//     const noteId = req.params.id;
+//     // const requestedNote = req.body.upvote;
+
+//     for (let i = 0; i < notes.length; i++) {
+//       const currentNote = notes[i];
+//       console.log(currentNote.note_id);
+//       if (currentNote == noteId) {
+//         res.json(currentNote);
+//       }
+//     }
+//     res.json('Review ID not found');
+//   }
+// });
 
 
 
@@ -39,10 +63,11 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
 
   // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
+  const title = req.body.title;
+  const text = req.body.text;
 
   // If all the required properties are present
-  if (title && text) {
+  if (title, text) {
     // Variable for the object we will save
     const newNote = {
       title,
@@ -50,67 +75,37 @@ router.post('/notes', (req, res) => {
       note_id: uniqid.process(),
     };
 
-    fs.readFile('../../db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        //convert string into objects
-        const parsedNotes = JSON.parse(data);
-        console.log(parsedNotes);
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
 
-        //add new note
-        parsedNotes.push(newNote);
 
-        //write note to db.json file
-        fs.writeFile(
-          '../../db/db.json',
-          //db/db.json
-        
-          JSON.stringify(parsedNotes, null, 3),
-          //maybe add err handling??
-        );
-      }
-    });
+    try {
+      const notes = fs.readFileSync('db/db.json', 'utf-8')
+      //convert string into objects
+      const parsedNotes = JSON.parse(notes);
+      console.log(parsedNotes);
 
-    // const response = {
-    //   status: 'success',
-    //   body: newNote,
-    // };
+      //add new note
+      parsedNotes.push(newNote);
+
+      //write note to db.json file
+      fs.writeFileSync('db/db.json', JSON.stringify(parsedNotes, null, 4));
+    }
+
+    catch (error) {
+      console.log(error);
+      res.json(error)
+    };
+    res.json(response);
+
 
     console.log(newNote);
-
-    res.json(newNote);
+    // res.json(newNote);
   } else {
     res.json('Error in adding a note');
   }
 });
-
-
-
-
-// router.post('/notes', (req, res) => {
-//   // set id based on what the next index of the array will be
-//   req.body.id = uniqid.process();
-
-//   // add animal to json file and animals array in this function
-//   const note = createNewNote(req.body, notes);
-
-//   res.json(note);
-// });
-
-
-
-
-
-
-// router.post('/notes', (req, res) => {
-//   //req.body is where incoming data will go
-  
-//   // create uniqId
-//   req.body.id = uniqid.process();
-//   res.json(req.body);
-//   console.log(req.json(req.body));
-// })
-
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 // import the express package
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes/noteRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 
@@ -11,16 +11,21 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001
 
 // initialize app variable / server
-const app =express();
+const app = express();
 
 
 
 // Middleware
-app.use(express.static('public'));
-// express.urlencoded({extended: true}) (built-in express method) takes incoming POST data & converts to key/value pairings to be accessed in  req.body object (extended means there may be a sub array so look deep!!)
-app.use(express.urlencoded({ extended: true }));
 // express.json() takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object
 app.use(express.json());
+
+// express.urlencoded({extended: true}) (built-in express method) takes incoming POST data & converts to key/value pairings to be accessed in  req.body object (extended means there may be a sub array so look deep!!)
+app.use(express.urlencoded({ extended: true }));
+// writes static HTMl pages from public dir
+app.use(express.static('public'));
+
+
+
 //directs what the routes are for / and api queries
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
